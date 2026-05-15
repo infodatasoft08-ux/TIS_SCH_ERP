09f683c072ec1bdc20b8fc8f68df972205f7d386
MayankMehar <mayank.mehar@yusata.com>
2026-05-15 10:32:07 +0530

    commit for adding redis password

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const redisOptions = {
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD,
     // maxRetriesPerRequest must be null for BullMQ
     maxRetriesPerRequest: null,
 };
